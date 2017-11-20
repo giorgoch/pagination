6964556e60f0ca15548bc27a88d836c20afc2692
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-11-20 22:12:39 +0200

    new paginationcontroller

---
+++ SpringDataPaginationDemo/src/main/java/com/sample/pagination/controller/PaginationController.java
@@ -0,0 +1,35 @@
+package com.sample.pagination.controller;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.pagination.dao.PeopleManangementRepository;
+import com.sample.pagination.model.Person;
+
+@RestController
+@RequestMapping("/api/")
+public class PaginationController {
+
+	@Autowired
+	private PeopleManangementRepository peopleManangementRepository;
+
+	@GetMapping("/pagination")
+	public ResponseEntity<?> getPaginatedResults(@PathParam("limit") int limit, @PathParam("offset") int offset,
+			@PathParam("firstname") String firstname) {
+		List<Person> person = peopleManangementRepository.findByFirstName(firstname,
+				new PageRequest(offset, limit, Direction.DESC, "firstName"));
+		return new ResponseEntity<>(person, HttpStatus.OK);
+	}
+
+}
