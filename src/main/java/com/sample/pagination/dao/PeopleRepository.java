87c54c3aee70970e9b8e1468d0df9601314e5b31
giorgoch <chatziefstratiougeorgios@gmail.com>
2017-11-20 22:55:12 +0200

    adding new file

---
+++ src/main/java/com/sample/pagination/dao/PeopleRepository.java
@@ -0,0 +1,13 @@
+package com.sample.pagination.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.sample.pagination.model.Person;
+
+public interface PeopleRepository extends PagingAndSortingRepository<Person, Integer> {
+	List<Person> findByFirstName(String firstFame, Pageable pageable);
+}
